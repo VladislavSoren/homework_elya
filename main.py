125a13aa755ae8bf2675eb593f3a6959857cceea
ellada2802 <ellada2802@gmail.com>
2026-07-13 16:47:57 +0300

    дз

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-print("hello")
-hello = "my hello"
-print(hello)
-
-# ДЗ
-"""
-123
-
-"""
+repeats = 1
+while repeats <= 5:
+    number = int(input("введите число от 1 до 10"))
+    if number in {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}:
+         print("число принято")
+         break
+    repeats += 1      
